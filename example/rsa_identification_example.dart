6d941e3b72b2ed3819adc35fc781a035d0fcae8e
marcus-bornman <marcus.bornman@frei.one>
2020-01-19 17:58:07 +0200

    Initial commit

---
+++ example/rsa_identification_example.dart
@@ -0,0 +1,6 @@
+import 'package:rsa_identification/rsa_identification.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/rsa_identification.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library rsa_identification;
+
+export 'src/rsa_identification_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
