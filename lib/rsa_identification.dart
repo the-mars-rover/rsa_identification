d793aa217150888937e4b84b7b2bb31a9fb2e9b7
marcus-bornman <marcus.bornman@frei.one>
2020-04-15 12:08:40 +0200

    Updated docs

--- lib/rsa_identification.dart
+++ lib/rsa_identification.dart
@@ -1,5 +1,5 @@
-/// A Dart Library for decoding and providing South African identification
-/// details from documents such as ID Books, ID Cards and Driver's licenses.
+/// A Dart Library for decoding and providing identification details from
+/// documents such as South African ID Books, ID Cards and Driver's licenses.
 library rsa_identification;
 
 export 'src/rsa_drivers_license.dart';
